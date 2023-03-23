7aeba7f944088a9459269c34f498a418c2286053
Aadroman <rubenlongeque@gmail.com>
2023-03-23 11:56:20 +0100

    passage en Singleton de la onnexion à la base de données

--- Belote2023/src/main/Belote.java
+++ Belote2023/src/main/Belote.java
@@ -16,48 +16,52 @@ import javax.swing.JOptionPane;
 
 public class Belote {
 
-	public static void main(String[] args) throws SQLException {
-		
-        Connection connection = null;  
-        Statement statement = null;  		
 
-		// Connection � la base de donn�es
-		// et cr�ation des champs 
-
-		try {
-			Class.forName("org.hsqldb.jdbcDriver").newInstance();
-
+	private String db ="";
+	private String url = "jdbc:hsqldb:file:";
+	private String user = "sa";
+	private String pwd = "";
+	private static Connection connect = null;
+	private Belote(){
+		try{
 			String dos = System.getProperty("user.dir");
 			String beloteFile = dos + "\\jBelote";
 			String createFile = dos + "\\create.sql";
+			Class.forName("org.hsqldb.jdbcDriver");
+			connect = DriverManager.getConnection(url+ beloteFile + "\\belote",user,pwd);
 			System.out.println("Dossier de stockage:" + beloteFile);
 			if( !new File(beloteFile).isDirectory() ){
 				new File(beloteFile).mkdir();
 			}
-			connection = DriverManager
-					.getConnection("jdbc:hsqldb:file:" + beloteFile + "\\belote","sa","");
-			statement = connection.createStatement();
-
-			importSQL(connection, new File(createFile));
-
+			importSQL(connect, new File(createFile));
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
 		}catch(SQLException e){
-				JOptionPane.showMessageDialog(null, "Impossible de se connecter à la base de donn�e. Vérifier qu'une autre instance du logiciel n'est pas déjà ouverte.");
-				System.out.println(e.getMessage());
-				System.exit(0);
-			}catch (Exception e) {  
-			    JOptionPane.showMessageDialog(null, "Erreur lors de l'initialisation du logiciel. Vérifiez votre installation Java et vos droits d'acc�s sur le dossier AppData.");
-			    System.out.println(e.getMessage());
-			    System.exit(0);
-			} 
-	        
-		// Interface graphique
-			
+			JOptionPane.showMessageDialog(null, "Impossible de se connecter à la base de donn�e. Vérifier qu'une autre instance du logiciel n'est pas déjà ouverte.");
+			System.out.println(e.getMessage());
+			System.exit(0);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Connection getUniqueInstanceBelote(){
+		if(connect==null)
+			new Belote();
+		return connect;
+	}
+
+	public static void main(String[] args) {
+		Connection connect = getUniqueInstanceBelote();
+		Statement statement = null;
+		try {
+			statement = connect.createStatement();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
 		Fenetre f = new Fenetre(statement);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//statement.execute("SHUTDOWN;");
-		//statement.close();  
-        //connection.close();  		
 	}
 
 	
